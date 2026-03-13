45a5c3fd069a7604efd203958613f93e7128404d
KarbonST <mikhailbykadorov@mail.ru>
2026-03-13 23:27:52 +0300

    chore: bootstrap Spring Boot backend with Flyway and PostgreSQL

---
+++ src/test/java/ru/vstu/medsim/MedsimBackApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.vstu.medsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedsimBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
